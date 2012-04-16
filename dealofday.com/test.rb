a25c7b44e4eada8ba4a9e40fe85e980ed00a433f
blue <blue0309@gmail.com>
2012-04-16 17:21:19 +0800

    update view

---
+++ dealofday.com/test.rb
@@ -0,0 +1,13 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+require "iconv"
+
+class TestSpider
+
+  date = Time.parse('Thu, 12 Apr 2012 14:24:36 +0000')
+  now = Time.now
+  d = now.to_i - date.to_i
+  p d/(60*60*24)
+end
+
